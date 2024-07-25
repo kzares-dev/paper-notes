9183070538f4dfd1383cd3188df0996fab56cf84
kzares <kzares.dev@gmail.com>
2024-07-25 17:51:59 -0400

    fixed & finished the add path functionality

--- src/components/react/AddPath.tsx
+++ src/components/react/AddPath.tsx
@@ -1,15 +1,26 @@
-import { updateRoute } from "utils/store"
+import { useStore } from "@nanostores/react"
+import { useEffect, useState } from "react"
+import {route, updateRoute } from "utils/store"
 
 const AddPath = ({ path } : { path: string}) => {
-  return (
-    <div onClick={() => updateRoute(path)} className="flex-1 flex items-center flex-col bg-gray-50 border h-fit py-5 rounded-md px-2 text-gray-700  text-[30px] font-bold hover:shadow-md cursor-pointer">
-         <img src="/images/add.svg" width={40} />
+  const $route = useStore(route)
+  const [itemSelected, setItemSelected] = useState($route.includes(path));
 
-         <h1>
+  const onButtonPress = () => {
+    updateRoute(path.toLowerCase())
+    setItemSelected(!itemSelected)
+  }
+
+  
+  return (
+    <button onClick={onButtonPress} className="flex-1  flex items-center justify-center flex-row gap-3  md:flex-col bg border w-full md:w-fit h-fit py-5 rounded-md px-2 text-gray-700  text-[30px] font-bold  cursor-pointer">
+        {itemSelected? <img src="/images/folder-check.svg" width={40} /> :  <img src="/images/add.svg" width={40} />}
+
+         {itemSelected ? <h1 className="text-orange-600">Selected</h1> : <h1>
          Select 
          <span className="font-black text-orange-600">{" " + path}</span>
-         </h1>
-    </div>
+         </h1>}
+    </button>
   )
 }
 
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,8 +1,16 @@
 import { atom } from "nanostores";
 
-export const route = atom<string[]>(["backend", "javascript", "java"]);
+export const route = atom<string[]>(["backend", "javascript"]);
 
 export function updateRoute(newRoute: string) {
+    
+    if(route.get().includes(newRoute)) {
+        const filtered =  route.get().filter((item) => item !== newRoute);
+        route.set(filtered);
+        return
+    };
+
     route.set([...route.get(), newRoute])
     console.log(route.get())
 }
+
